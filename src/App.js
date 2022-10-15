2eeb9b6961178023e7ffa22bd43be388227236b8
prasadvinnakota <prasadvinnakota6@gmail.com>
2022-10-15 10:25:01 +0530

    :added a new place in each state
    added a description property to each place

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import { useState } from 'react';
 
 function App() {
   
-  var andhraPlaces=[{name:"Vishakapatnam",rating:"5"},{name:"Kakinada",rating:"4"}]
-  var telanganaPlaces=[{name:"Hyderabad",rating:"4.5"},{name:"Warrangal",rating:"4"}]
-  var tamilnaduPlaces=[{name:"Arunachalam", rating:"4"},{name:"Rameswaram",rating:"4"}]
+  var andhraPlaces=[{name:"Vishakapatnam",description:"Vizag is the most well known beach place in state", rating:"5"},{name:"Kakinada",description:"Kakinada is a well known place for temples",rating:"4"},{name:"Palakollu", description:"Palakollu is my native place. I love to be here.", rating:"4"}]
+  var telanganaPlaces=[{name:"Hyderabad",description:"Hyderabed is the metropolitan city. It is the  IT HUB of Telangana",rating:"4.5"},{name:"Warrangal",description:"Warrangalhas many historica places. It is our history of Kings",rating:"4"},{name:"Secundrabaed",description:"Secundrabed is the twin city. It is the  IT TWIN of Telangana",rating:"4.5"}]
+  var tamilnaduPlaces=[{name:"Arunachalam",description:"Arunachalam is the best place i feel beacause its lord shiva place",rating:"4"},{name:"Rameswaram",description:"Rameswaram is the best place i feel beacause its lord shiva pace",rating:"4"},{name:"Madurai",description:"Madhurai is the place of nature",rating:"4"}]
 
   var [places, setPlaces] = useState([])
 
@@ -40,6 +40,7 @@ function App() {
       {
         return <li className="list__item">
         <h2>{place.name}</h2>
+        <h3>{place.description}</h3>
         <h3>{place.rating}/5</h3>
         </li>
       })}
